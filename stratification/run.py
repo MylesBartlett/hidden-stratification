af6bb26d10aa713544df5694c1eca9d340be6dc9
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-03-15 18:52:40 +0000

    Include dataset in default wandb group.

--- stratification/run.py
+++ stratification/run.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 
 import torch
+import wandb
 
 from stratification.harness import GEORGEHarness
 from stratification.utils.parse_args import get_config
 from stratification.utils.utils import init_cuda, set_seed
-import wandb
 
 
 def main():
@@ -26,7 +26,7 @@ def main():
         dir=str(local_dir),
         config=config,
         reinit=True,
-        group=config.get("group", config["dataset"]),
+        group=config.get("group", f"{config['dataset']}/{config['cluster_config']['method']}"),
     )
     torch.multiprocessing.set_sharing_strategy("file_system")
     harness = GEORGEHarness(config, use_cuda=use_cuda)
@@ -39,23 +39,9 @@ def main():
 
     activ_done = config["activations_dir"] != "NONE"
     rep_done = config["representation_dir"] != "NONE"
-    cluster_done = config["cluster_dir"] != "NONE"
-    rep_done = (
-        rep_done or cluster_done
-    )  # if we already have clusters, don't need to do reduction step
     activ_done = (
         activ_done or rep_done
     )  # don't need to get activations if we already have reduced ones
-    if config["classification_config"]["eval_only"]:
-        assert activ_done
-        if config["cluster_dir"] != "NONE":
-            dataloaders = harness.get_dataloaders(
-                config,
-                mode=first_mode,
-                subclass_labels=os.path.join(config["cluster_dir"], "clusters.pt")
-                if os.path.isdir(config["cluster_dir"])
-                else config["cluster_dir"],
-            )
 
     # Train a model with ERM
     if activ_done and not (
@@ -78,48 +64,27 @@ def main():
             config["classification_config"], model, dataloaders, mode=first_mode
         )
 
-    if (
-        config["classification_config"]["eval_only"]
-        or config["classification_config"]["save_act_only"]
-    ):
-        exit()
+    if not config["classification_config"]["bit_pretrained"] and not rep_done:
+        model.load_state_dict(torch.load(os.path.join(erm_dir, "best_model.pt"))["state_dict"])
 
-    if config["mode"] == "george":
-        if not config["classification_config"]["bit_pretrained"] and not rep_done:
-            model.load_state_dict(torch.load(os.path.join(erm_dir, "best_model.pt"))["state_dict"])
+    set_seed(config["seed"], use_cuda)
+    # Dimensionality-reduce the model activations
+    if rep_done:
+        reduction_dir = config["representation_dir"]
+    else:
+        reduction_model = harness.get_reduction_model(config, nn_model=model)
+        reduction_dir = harness.reduce(
+            config["reduction_config"],
+            reduction_model,
+            inputs_path=os.path.join(erm_dir, "outputs.pt"),
+        )
 
-        set_seed(config["seed"], use_cuda)
-        # Dimensionality-reduce the model activations
-        if rep_done:
-            reduction_dir = config["representation_dir"]
-        else:
-            reduction_model = harness.get_reduction_model(config, nn_model=model)
-            reduction_dir = harness.reduce(
-                config["reduction_config"],
-                reduction_model,
-                inputs_path=os.path.join(erm_dir, "outputs.pt"),
-            )
-
-        # Cluster the per-superclass features
-        if cluster_done:
-            cluster_dir = config["cluster_dir"]
-        else:
-            cluster_model = harness.get_cluster_model(config)
-            cluster_dir = harness.cluster(
-                config["cluster_config"],
-                cluster_model,
-                inputs_path=os.path.join(reduction_dir, "outputs.pt"),
-            )
-
-        # clusters = torch.load(os.join(cluster_dir, "clusters.pt"))
-        # set_seed(config['seed'], use_cuda)  # reset random state
-        # dataloaders = harness.get_dataloaders(
-        #     config, mode='george', subclass_labels=os.path.join(cluster_dir, 'clusters.pt'))
-        # model = harness.get_nn_model(config, num_classes=num_classes, mode='george')
-
-        # # Train the final (GEORGE) model
-        # george_dir = harness.classify(config['classification_config'], model, dataloaders,
-        # mode='george')
+    cluster_model = harness.get_cluster_model(config)
+    harness.cluster(
+        config["cluster_config"],
+        cluster_model,
+        inputs_path=os.path.join(reduction_dir, "outputs.pt"),
+    )
 
 
 if __name__ == "__main__":
